37d87f1cf08adec322102c4cfd0ecbe0481c7c32
Yusuke Oshiro <yusuke@oshiro1.com>
2017-12-18 00:03:47 +0900

    subscribe ir request

--- lib/subscribe.rb
+++ lib/subscribe.rb
@@ -7,11 +7,26 @@ require 'json'
 
 Dotenv.load
 
+def send_ir_signal( payload  )
+	url = ENV["IRKIT_HOST"] + "/messages"
+	uri = URI.parse url
+	https = Net::HTTP.new(uri.host, uri.port)
+	https.use_ssl = false 
+	req = Net::HTTP::Post.new(uri.request_uri)
+	req["Content-Type"] = "text/plain"
+	req["X-Requested-With"] = "curl"
+	req.body = payload
+	res = https.request(req)
+end 
+
+
 $redis = Redis.new(url: ENV["REDIS_URL"])
 
-$redis.subscribe('channel_test') do |on|
+$redis.subscribe('ir_request') do |on|
 	on.message do |channel, msg|
-		p msg
+		send_ir_signal msg
 	end
 end
+
+
 
