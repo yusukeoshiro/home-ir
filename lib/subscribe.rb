0fd9fe2016c5c63a951497cbd566c7e223e58449
Yusuke Oshiro <yusuke@oshiro1.com>
2018-09-04 08:52:09 +0000

    added try catch

--- lib/subscribe.rb
+++ lib/subscribe.rb
@@ -4,14 +4,15 @@ require "net/https"
 require "dotenv"
 require "redis"
 require 'json'
-
+require "pp"
 Dotenv.load
 
-def send_ir_signal( payload  )
-	url = ENV["IRKIT_HOST"] + "/messages"
+def send_ir_signal( ip_address, payload )
+	url = "http://" + ip_address + "/messages"
 	uri = URI.parse url
 	https = Net::HTTP.new(uri.host, uri.port)
 	https.use_ssl = false 
+	https.open_timeout = 2
 	req = Net::HTTP::Post.new(uri.request_uri)
 	req["Content-Type"] = "text/plain"
 	req["X-Requested-With"] = "curl"
@@ -24,9 +25,15 @@ $redis = Redis.new(url: ENV["REDIS_URL"])
 
 $redis.subscribe('ir_request') do |on|
 	on.message do |channel, msg|
-		p "detected new signal request..."
-		p msg
-		send_ir_signal msg
+		begin
+                	p "detected new signal request..."
+                	p msg
+               		payload = JSON.parse msg
+                	pp payload
+                	send_ir_signal( payload["emitter_ip_address"], payload["signal_body"] )
+		rescue
+			p "failed"
+		end
 	end
 end
 
